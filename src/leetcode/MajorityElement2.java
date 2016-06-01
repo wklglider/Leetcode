ec56d8fc346993e7c49cc7cce726e4d070e49359
wklglider <wklglider@gmail.com>
2016-06-01 14:48:03 -0600

    2ms solution

--- src/leetcode/MajorityElement2.java
+++ src/leetcode/MajorityElement2.java
@@ -2,8 +2,7 @@ package leetcode;
 
 public class MajorityElement2 {
 	  public int majorityElement(int[] nums) {
-	        int maj=0;
-		int count = 0;
+	        int maj = 0, count = 0;
 		for (int x : nums){
 		    if (count == 0){
 		        maj = x;
