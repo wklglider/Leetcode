95271238bf8555a4851678c8d3c7250c5184f7ac
wklglider <wklglider@gmail.com>
2016-06-03 18:06:46 -0600

    Update MergeTwoSortedLists2.java

--- src/leetcode/MergeTwoSortedLists2.java
+++ src/leetcode/MergeTwoSortedLists2.java
@@ -7,21 +7,15 @@ package leetcode;
  */
 public class MergeTwoSortedLists2 {
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-		if (l1 == null && l2 == null)
-			return null;
-		if (l1 == null)
-			return l2;
-		if (l2 == null)
-			return l1;
-
-		ListNode p = new ListNode(0);
+		if (l1 == null && l2 == null) return null;
+		if (l1 == null) return l2;
+		if (l2 == null) return l1;
 		if (l1.val < l2.val) {
-			p.next = l1;
-			l1.next = mergeTwoLists(l1.next, l2);
+		    l1.next = mergeTwoLists(l1.next, l2);
+		    return l1;
 		} else {
-			p.next = l2;
-			l2.next = mergeTwoLists(l1, l2.next);
+		    l2.next = mergeTwoLists(l1, l2.next);
+		    return l2;
 		}
-		return p.next;
 	}
 }
