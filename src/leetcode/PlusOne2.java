9fda0154ba3858973379a085134fba77515e9814
wklglider <wklglider@gmail.com>
2016-06-16 17:05:57 -0600

    Update PlusOne2.java

--- src/leetcode/PlusOne2.java
+++ src/leetcode/PlusOne2.java
@@ -5,22 +5,17 @@ package leetcode;
  */
 public class PlusOne2 {
 	public int[] plusOne(int[] digits) {
-		int carry = 1;
 		for (int i = digits.length - 1; i >= 0; i--) {
 			if (digits[i] < 9) {
-				digits[i] += carry;
+				digits[i]++;
 				return digits;
 			} else {
 				digits[i] = 0;
-				carry = 1;
 			}
 		}
 		// case digits = 999
-		if (carry == 1) {
-			int res[] = new int[digits.length + 1];
-			res[0] = 1;
-			return res;
-		}
-		return digits;
+		int res[] = new int[digits.length + 1];
+		res[0] = 1;
+		return res;
 	}
 }
